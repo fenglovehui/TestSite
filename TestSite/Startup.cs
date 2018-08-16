90b47913709d8d3904527a82b4cf458a1f413040
fenglovehui <38254667@qq.com>
2018-08-16 09:18:03 +0800

    commit

---
+++ TestSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestSite.Startup))]
+namespace TestSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
